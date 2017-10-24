4c98a252252d094e288e9f3f8a55885d424ffc74
Evan <ehutch7@gmail.com>
2017-10-23 23:24:32 -0400

    added projects and about me tabs along with some basic animations

--- src/bundle.js
+++ src/bundle.js
@@ -74,8 +74,20 @@ var $ = __webpack_require__(1);
 
 $(document).ready(function () {
 
-  $('.trigger').on('click', function () {
-    $(this).toggleClass('clicked');
+  $('.work').on('animationend webkitAnimationEnd mozAnimationEnd', function () {
+    $(this).removeClass('blastAnim');
+  });
+
+  $('.work').on('mouseover', function () {
+    $(this).addClass('blastAnim');
+  });
+
+  $('.about').on('animationend webkitAnimationEnd mozAnimationEnd', function () {
+    $(this).removeClass('blastAnim');
+  });
+
+  $('.about').on('mouseover', function () {
+    $(this).addClass('blastAnim');
   });
 });
 
--- src/index.js
+++ src/index.js
@@ -2,7 +2,20 @@ var $ = require('jquery');
 
 $(document).ready(function(){
 
-  $('.trigger').on('click', function(){
-      $(this).toggleClass('clicked');
+  $('.work').on('animationend webkitAnimationEnd mozAnimationEnd', function(){
+    $(this).removeClass('blastAnim');
   })
+
+  $('.work').on('mouseover', function(){
+      $(this).addClass('blastAnim');
+  })
+
+  $('.about').on('animationend webkitAnimationEnd mozAnimationEnd', function () {
+    $(this).removeClass('blastAnim');
+  })
+
+  $('.about').on('mouseover', function () {
+    $(this).addClass('blastAnim');
+  })
+
 });
