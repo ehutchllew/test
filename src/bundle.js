d5c74c875d1689a90383545d48aede286d4b3713
Evan <ehutch7@gmail.com>
2017-11-03 17:39:18 -0400

    Finding device type in the JS file and altering behavior accordingly, also updating animations for the hamburger menu icon and menu popout

--- src/bundle.js
+++ src/bundle.js
@@ -75,15 +75,31 @@ var $ = __webpack_require__(1);
 $(document).ready(function () {
   var $main = $('.main');
   var $hamburger = $('.hamburger');
+  var $theMenu = $('.theMenu');
 
-  $main.on('mouseover', function () {
-    $(this).addClass('blastAnim').on('animationend webkitAnimationEnd mozAnimationEnd', function () {
-      $(this).removeClass('blastAnim');
+  var regStr = /Android|iPhone|iPad/i;
+  var deviceType = navigator.userAgent;
+  console.log(regStr.exec(deviceType));
+
+  if (regStr.test(deviceType)) {
+    $main.on('click', function () {
+      $(this).addClass('blastAnim').on('animationend webkitAnimationEnd mozAnimationEnd', function () {
+        $(this).removeClass('blastAnim');
+      });
     });
-  });
+  } else {
+    $main.on('mouseover', function () {
+      $(this).addClass('blastAnim').on('animationend webkitAnimationEnd mozAnimationEnd', function () {
+        $(this).removeClass('blastAnim');
+      });
+    });
+  }
 
   $hamburger.on('click', function () {
+    $hamburger.find('.trigger').toggleClass('expand');
     $hamburger.find('.filling').toggleClass('expand');
+
+    $theMenu.toggleClass('expand');
   });
 });
 
--- src/index.js
+++ src/index.js
@@ -3,15 +3,32 @@ var $ = require('jquery');
 $(document).ready(function(){
   var $main = $('.main');
   var $hamburger = $('.hamburger');
-  
-  $main.on('mouseover', function(){
-    $(this).addClass('blastAnim').on('animationend webkitAnimationEnd mozAnimationEnd', function () {
-      $(this).removeClass('blastAnim');
+  var $theMenu = $('.theMenu');
+
+  var regStr = /Android|iPhone|iPad/i;
+  var deviceType = navigator.userAgent;
+  console.log(regStr.exec(deviceType));
+
+  if(regStr.test(deviceType)){
+    $main.on('click', function(){
+      $(this).addClass('blastAnim').on('animationend webkitAnimationEnd mozAnimationEnd', function () {
+        $(this).removeClass('blastAnim');
+      });
     });
-  });
+  } else {
+    $main.on('mouseover', function(){
+      $(this).addClass('blastAnim').on('animationend webkitAnimationEnd mozAnimationEnd', function () {
+        $(this).removeClass('blastAnim');
+      });
+    });
+  }
+
 
   $hamburger.on('click', function(){
+    $hamburger.find('.trigger').toggleClass('expand');
     $hamburger.find('.filling').toggleClass('expand');
+
+    $theMenu.toggleClass('expand');
   });
 
   
