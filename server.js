097fec8fcf2b20b9c8298d15526fb832cd0972aa
Evan <ehutch7@gmail.com>
2017-11-03 19:20:25 -0400

    changing the order of some files to hopefully get them working on github pages

--- server.js
+++ server.js
@@ -2,9 +2,9 @@ var express = require('express');
 var app = express();
 var path = require('path');
 
-app.use(express.static(path.join(__dirname + '/src')));
+app.use(express.static(path.join(__dirname)));
 app.get('/', function(request, response){
-    response.sendFile(__dirname + '/src/index.html');
+    response.sendFile(__dirname + '/index.html');
 
 });
 
